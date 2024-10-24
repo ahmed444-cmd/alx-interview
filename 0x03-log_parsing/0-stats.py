9f241c38bdd30c8f6437381f73271269c0783349
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-10-24 15:34:25 -0700

    Mandatory

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Write a script that reads stdin line by line and computes metrics:
+"""
+Develop a script that reads stdin line by line to compute metrics:
 
 Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size> (if the format is not this one, the line
-must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning:
+<status code> <file size> (skip the line if the format does
+not match this one)
+After every 10 lines or a keyboard interruption (CTRL + C),
+output these statistics from the start:
 Total file size: File size: <total size>
 where <total size> is the sum of all previous <file size>
-(see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer,
-don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
+(based on the format outlined above)
+Count of lines by status code:
+Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500
+If a status code is missing or non-integer,
+don’t display anything for that status code
+format: <status code>: <count>
+Status codes must be printed in ascending order
 
 line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
 <status code>, <file size>]
 """
-
 
 import sys
 
