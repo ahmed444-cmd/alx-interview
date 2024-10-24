b5b08e7c7ce946baa998fee60cd7fff8614b014d
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-10-24 15:22:28 -0700

    Mandatory

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Task 0. Log parsing
+
+A script that processes stdin line by line and computes metrics.
+"""
+
+import sys
+
+
+def printStats(file_size, status):
+"""
+printStats
+
+    This function prints the total file size and the
+    statuses that were invoked.
+
+    Arguments:
+        file_size (int): The total file size to be printed.
+        status (dict{int, int}): A dictionary of the statuses invoked.
+"""
+
+print("File size: {}".format(file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+total_file_size = 0
+count = 0
+possible_status = {200: 0, 301: 0, 400: 0, 401: 0,
+                   403: 0, 404: 0, 405: 0, 500: 0}
+try:
+    for line in sys.stdin:
+        args = line.split()
+
+        status_code = int(args[-2])
+        file_size = int(args[-1])
+
+        if status_code in possible_status:
+            possible_status[status_code] += 1
+
+        total_file_size += file_size
+        count += 1
+
+        if count == 10:
+            printStats(total_file_size, possible_status)
+            count = 0
+    printStats(total_file_size, possible_status)
+except KeyboardInterrupt:
+    raise
+finally:
+    printStats(total_file_size, possible_status)
