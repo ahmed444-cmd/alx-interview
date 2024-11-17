e14c513aec17e06f22dc7f6dba1672af2fe9b2b3
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-11-17 09:06:13 -0800

    Mandatory

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,49 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-     	 req(arr, i + 1);
-    }
-  });
-};
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie_ID>');
+  process.exit(1);
+}
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-	      req(chars, 0);
-    }
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
   }
-	);
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to retrieve movie information. Status Code:', response.statusCode);
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
+  async function getCharacterData (characterUrl) {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(`Failed to retrieve character information. Status Code: ${response.statusCode}`);
+        } else {
+          resolve(JSON.parse(body));
+        }
+      });
+    });
+  }
+(async function printCharacters () {
+    for (const characterUrl of characters) {
+      try {
+        const characterData = await getCharacterData(characterUrl);
+        console.log(characterData.name);
+      } catch (error) {
+        console.error('Error:', error);
+        process.exit(1);
+      }
+    }
+  })();
+});
