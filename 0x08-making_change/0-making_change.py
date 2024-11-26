5ce1f24705ee0f8d049bfcec62bfba64473b33c7
Ahmed El Abid <ahmedelabide@gmail.com>
2024-11-26 02:43:30 -0800

    Mandatory

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,23 +2,23 @@
 """Making Change"""
 
 def makeChange(coins, total):
-    """Making Change"""
+    """ Generate changes needed to reach total
+
+    Args:
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
+    """
     if total <= 0:
         return 0
-    elif total > 0:
-        newList = sorted(coins[:])
-        newList = list(reversed(newList))
-        count = 0
-        value = total + 0
-        index = 0
-        while value >= 0 and (index < len(newList)):
-            if value >= newList[index]:
-                value = value - newList[index]
-                count += 1
-            elif value < newList[index]:
-                index += 1
-        if index == len(newList):
-            if value != 0:
-                return -1
-            elif value == 0:
-                return count
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
