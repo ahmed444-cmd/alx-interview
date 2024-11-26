b5ff0f699e605c4c20ba53cfd8657b6bb20afa29
Ahmed El Abid <ahmedelabide@gmail.com>
2024-11-26 03:15:48 -0800

    Mandatory

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,8 +4,7 @@
 
 
 def makeChange(coins, total):
-    """Return fewest number of coins needed to meet total
-    0 if total is 0 or less, -1 if total cannot be met
+    """Return the fewest coins needed; 0 if total ≤ 0, -1 if impossible.
     """
     if total <= 0:
         return 0
