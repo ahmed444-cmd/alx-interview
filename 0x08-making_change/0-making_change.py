0c12b839a905872dd283463e29181fa410bcb859
Ahmed El Abid <ahmedelabide@gmail.com>
2024-11-26 02:46:27 -0800

    Mandatory

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,27 @@
-#!/usr/bin/python3
-"""Making Change"""
-
 def makeChange(coins, total):
-    """ Generate changes needed to reach total"""
+    """
+    Determine the fewest number of coins needed to meet a given amount total.
+
+    :param coins: List of coin values.
+    :param total: Target total amount.
+    :return: Fewest number of coins needed, or -1 if not possible.
+    """
     if total <= 0:
         return 0
-    check = 0
-    temp = 0
+
+    # Sort coins in descending order to start with the largest denomination
     coins.sort(reverse=True)
-    for i in coins:
-        while check < total:
-            check += i
-            temp += 1
-        if check == total:
-            return temp
-        check -= i
-        temp -= 1
-    return -1
+
+    # Initialize variables
+    count = 0
+    remaining = total
+
+    for coin in coins:
+        if remaining <= 0:
+            break
+        # Use as many of this coin as possible
+        count += remaining // coin
+        remaining %= coin
+
+    # If there's any amount remaining, return -1
+    return count if remaining == 0 else -1
